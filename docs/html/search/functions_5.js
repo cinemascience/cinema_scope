2aa18777f159a28d5089856bce5b97ebd1a6dcb7
David Honegger Rogers <dhr@lanl.gov>
2018-12-08 06:51:17 -0700

    adding docs for github webpage; installers

---
+++ docs/html/search/functions_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['mousemoveevent',['mouseMoveEvent',['../class_cin_image_view.html#a389afefbc32f210a8c0071d6fb5959b5',1,'CinImageView']]],
+  ['mousepressevent',['mousePressEvent',['../class_cin_image_view.html#a7564f9cf0d0517c4e9b2b0bb2ada3d5d',1,'CinImageView']]],
+  ['mousereleaseevent',['mouseReleaseEvent',['../class_cin_image_view.html#a63d367e2e0d2c1d49cc9114132521031',1,'CinImageView']]]
+];
---
+++ docs/html/search/functions_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['recordvalue',['recordValue',['../class_cin_parameter.html#ae450780ab328db4d5e60e7ca243a20a9',1,'CinParameter']]]
+];
