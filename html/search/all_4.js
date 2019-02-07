192895e7177d2892848b89e64cd27a98d3d84d34
David Honegger Rogers <dhr@lanl.gov>
2019-02-07 11:09:50 -0700

    updated docs

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['getnamefortype',['GetNameForType',['../class_cin_parameter.html#ab440298399882f5ea33c21ea2db70261',1,'CinParameter']]],
-  ['getnextvalue',['getNextValue',['../class_cin_parameter.html#a80744f864ed0f7be51e74fc5fa7596a9',1,'CinParameter']]]
+  ['getnamefortype',['GetNameForType',['../class_cin_parameter.html#ab440298399882f5ea33c21ea2db70261',1,'CinParameter']]]
 ];
