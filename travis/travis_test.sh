3f8dc25df4d11d808612eb3db6c8410bb42ee82a
Ethan Stam <ethan.stam@gmail.com>
2019-02-12 11:35:55 -0700

    Looking for test executable

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -4,9 +4,9 @@ set -e
 case $TRAVIS_OS_NAME in
     linux)
         echo "*******"
-        ls $TRAVIS_BUILD_DIR/build
+        ls -l $TRAVIS_BUILD_DIR/build/debug
         echo "*******"
-        find $TRAVIS_BUILD_DIR/build/debug -name "*test*"
+        $TRAVIS_BUILD_DIR/cinema_scope_test
         ;;
     osx)
         ;;
