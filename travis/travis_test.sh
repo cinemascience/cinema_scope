ff58bf83d83b79d52b0a8e1ae392eb33703ef6e8
Ethan Stam <ethan.stam@gmail.com>
2019-02-12 14:03:43 -0700

    Moving test to travis_test.sh

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -3,13 +3,19 @@ set -e
 
 case $TRAVIS_OS_NAME in
     linux)
-        echo "*******"
-        ls -l $TRAVIS_BUILD_DIR/build/debug
-        echo "*******"
-        #xvfb-run $TRAVIS_BUILD_DIR/cinema_scope_test
+        # cd to a next level directory because of the relative path in /src/CinScopeTest.cpp
+        pushd $TRAVIS_BUILD_DIR/src
+        # find the test executable, run it, and quit so it's not run more than once. xargs is used to get the exit code of the tests
+        find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
+        popd
         ;;
     osx)
+        pushd $TRAVIS_BUILD_DIR/src
+        popd
         ;;
     windows)
+        pushd $TRAVIS_BUILD_DIR/src
+        find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
+        popd
         ;;
 esac
