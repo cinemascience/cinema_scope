7f4b4168e91bfe830cbe40a2280b9a48f0e0833a
Ethan Stam <ethan.stam@gmail.com>
2019-02-12 14:17:42 -0700

    Trying cd instead of pushd for macos builds

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -4,18 +4,18 @@ set -e
 case $TRAVIS_OS_NAME in
     linux)
         # cd to a next level directory because of the relative path in /src/CinScopeTest.cpp
-        pushd $TRAVIS_BUILD_DIR/src
+        cd $TRAVIS_BUILD_DIR/src
         # find the test executable, run it, and quit so it's not run more than once. xargs is used to get the exit code of the tests
         find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
-        popd
+        cd $TRAVIS_BUILD_DIR
         ;;
     osx)
-        pushd $TRAVIS_BUILD_DIR/src
-        popd
+        cd $TRAVIS_BUILD_DIR/src
+        cd $TRAVIS_BUILD_DIR
         ;;
     windows)
-        pushd $TRAVIS_BUILD_DIR/src
+        cd $TRAVIS_BUILD_DIR/src
         find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
-        popd
+        cd $TRAVIS_BUILD_DIR
         ;;
 esac
