65835e44b0a3720859d95c735d4cf103ae56074b
Ethan Stam <ethan.stam@gmail.com>
2019-02-12 11:23:30 -0700

    Trying different build directory

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -3,10 +3,10 @@ set -e
 
 case $TRAVIS_OS_NAME in
     linux)
-        echo *******
-        ls $TRAVIS_BUILD_DIR
-        echo *******
-        $TRAVIS_BUILD_DIR/cinema_scope_test/bin/cinema_scope_test
+        echo "*******"
+        ls $TRAVIS_BUILD_DIR/build
+        echo "*******"
+        $TRAVIS_BUILD_DIR/build/debug/bin/cinema_scope_test
         ;;
     osx)
         ;;
