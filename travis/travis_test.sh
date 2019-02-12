4ab672914a171ed9b11166e572c9618da63dd3ea
Ethan Stam <ethan.stam@gmail.com>
2019-02-12 12:19:57 -0700

    Trying xvfb run in .travis.yml

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -6,7 +6,7 @@ case $TRAVIS_OS_NAME in
         echo "*******"
         ls -l $TRAVIS_BUILD_DIR/build/debug
         echo "*******"
-        xvfb-run $TRAVIS_BUILD_DIR/cinema_scope_test
+        #xvfb-run $TRAVIS_BUILD_DIR/cinema_scope_test
         ;;
     osx)
         ;;
