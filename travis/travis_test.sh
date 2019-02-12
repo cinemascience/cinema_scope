5e155b4a02d76295669e7e1f076409bbbf2b083f
Ethan Stam <ethan.stam@gmail.com>
2019-02-12 14:25:49 -0700

    Trying xargs -0 bash... without -c

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -6,7 +6,7 @@ case $TRAVIS_OS_NAME in
         # cd to a next level directory because of the relative path in /src/CinScopeTest.cpp
         cd $TRAVIS_BUILD_DIR/src
         # find the test executable, run it, and quit so it's not run more than once. xargs is used to get the exit code of the tests
-        find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
+        find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash 2>&1
         cd $TRAVIS_BUILD_DIR
         ;;
     osx)
@@ -15,7 +15,7 @@ case $TRAVIS_OS_NAME in
         ;;
     windows)
         cd $TRAVIS_BUILD_DIR/src
-        find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
+        find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash 2>&1
         cd $TRAVIS_BUILD_DIR
         ;;
 esac
