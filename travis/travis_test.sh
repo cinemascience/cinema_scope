4d2aa25c18997bf82378b05128b45fece665b3aa
Ethan Stam <ethan.stam@gmail.com>
2019-02-13 12:12:48 -0700

    Reactivating all other os builds All should fail on the tests resulting in a failure for the whole build

--- travis/travis_test.sh
+++ travis/travis_test.sh
@@ -4,13 +4,13 @@ set -e
 case $TRAVIS_OS_NAME in
     linux)
         # cd to a next level directory because of the relative path in /src/CinScopeTest.cpp
-        cd $TRAVIS_BUILD_DIR/src
+        #cd $TRAVIS_BUILD_DIR/src
         # find the test executable, run it, and quit so it's not run more than once. xargs is used to get the exit code of the tests
         find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
         cd $TRAVIS_BUILD_DIR
         ;;
     osx)
-        cd $TRAVIS_BUILD_DIR/src
+        #cd $TRAVIS_BUILD_DIR/src
         find ../ -name "cinema_scope_test" -print0 -quit | xargs -0 bash -c 2>&1
         cd $TRAVIS_BUILD_DIR
         ;;
