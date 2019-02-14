90eca2bec0627576e007e5417b2a2595dd2830f0
Ethan Stam <ethan.stam@gmail.com>
2019-02-14 11:53:53 -0700

    REPO_SLUG to get ready for pull request

--- travis/travis_deploy.sh
+++ travis/travis_deploy.sh
@@ -6,13 +6,13 @@ case $TRAVIS_OS_NAME in
         ;;
     osx)
         if [[ "$osx_image" == "xcode10" ]]; then
-          git clone "https://github.com/EthanS94/cinema_scope"
+          git clone "https://github.com/${TRAVIS_REPO_SLUG}"
           cd cinema_scope
           doxygen ./doxyfile.cfg
           git commit -am "Updating doxygen"
           git subtree split --prefix docs -b gh-pages
           git checkout gh-pages
-          git push --force "https://${GITHUB_TOKEN}@github.com/EthanS94/cinema_scope" HEAD:gh-pages
+          git push --force "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" HEAD:gh-pages
         fi
         ;;
     windows)
