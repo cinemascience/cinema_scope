1794f6a1aa6d490ab1a6da569d56c6ab241d833d
Ethan Stam <ethan.stam@gmail.com>
2019-02-14 12:57:49 -0700

    Hopefully the correct way to update gh-pages and retain the doc history

--- travis/travis_deploy.sh
+++ travis/travis_deploy.sh
@@ -6,13 +6,16 @@ case $TRAVIS_OS_NAME in
         ;;
     osx)
         if [[ "$osx_image" == "xcode10" ]]; then
-          git clone --depth 1 "https://github.com/${TRAVIS_REPO_SLUG}"
+          git clone "https://github.com/${TRAVIS_REPO_SLUG}"
           cd cinema_scope
+          git checkout -b gh-pages origin/gh-pages
+          git checkout master
           doxygen ./doxyfile.cfg
           git commit -am "Updating doxygen"
-          git subtree split --prefix docs -b gh-pages
+          git subtree split --prefix docs -b gh-pages-new
           git checkout gh-pages
-          git push --force "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" HEAD:gh-pages
+          git merge gh-pages-new
+          git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" HEAD:gh-pages
         fi
         ;;
     windows)
